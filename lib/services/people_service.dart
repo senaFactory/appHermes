c7c3fe1bab9f187205bcda83de5a8c46be3dc795
NymScript <niyimorera28@gmail.com>
2024-10-22 10:47:36 -0500

    URL siguiendo el puerto del backend para dis. fisicos

--- lib/services/people_service.dart
+++ lib/services/people_service.dart
@@ -3,13 +3,14 @@ import 'package:http/http.dart' as http;
 import 'package:maqueta/models/user.dart';
 
 class PeopleService {
-  final String baseUrl = 'http://10.0.2.2:8081/api/v1/hermesapp/people';
+  final String baseUrl = 'https://hhj97mdq-8081.use2.devtunnels.ms/api/v1/hermesapp/people';
 
   Future<User?> getUserById(int id) async {
     final url = Uri.parse('$baseUrl/by-id/$id');
 
     try {
       final response = await http.get(url);
+      print(response);
 
       if (response.statusCode == 200) {
         final Map<String, dynamic> jsonResponse = json.decode(response.body);
@@ -27,7 +28,7 @@ class PeopleService {
             name: userData['name'] ?? 'N/A',
             lastName: userData['lastname'] ?? 'N/A',
             email: userData['email'] ?? 'N/A',
-            phoneNumber: userData['phoneNumber'] ?? '',
+            phoneNumber: userData['phone'] ?? '',
             bloodType: userData['bloodType'] ?? 'N/A',
             documentNumber: userData['document'].toString(),
             documentType: userData['documentType'] ?? 'N/A',
