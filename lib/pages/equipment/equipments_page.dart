52b19cbee042dcf1b2935613b82af401bb6e5f7b
Daniel Ramirez <dsramirez483@soy.sena.edu.co>
2024-10-31 17:21:06 -0500

    Final touches for Show Equipments

--- lib/pages/equipment/equipments_page.dart
+++ lib/pages/equipment/equipments_page.dart
@@ -34,115 +34,129 @@ class _EquipmentspageState extends State<Equipmentspage> {
 
   @override
   Widget build(BuildContext context) {
-    // Si se pasó un equipo nuevo, lo añadimos a la lista
-
     return Scaffold(
-      body: _equipments.isEmpty
-          ? Column(
-              children: [
-                const HomeAppBar(),
-                const SizedBox(height: 30),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const AddEquipmentPage(),
-                      ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFF39A900),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 10),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const HomeAppBar(),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Mis Equipos",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFF39A900),
                     ),
                   ),
-                  child: const Text(
-                    "Agregar equipo",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ],
-            )
-          : Column(
-              children: [
-                const HomeAppBar(),
-                const SizedBox(height: 40),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        "Mis Equipos",
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w600,
-                          color: Color(0xFF39A900),
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () async {
-                          final newEquipment = await Navigator.push<Equipment>(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const AddEquipmentPage(),
-                            ),
-                          );
-
-                          if (newEquipment != null) {
-                            setState(() {
-                              _equipments.add(newEquipment);
-                            });
-                          }
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFF39A900),
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 10),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                        child: const Text(
-                          "Agregar equipo",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Expanded(
-                  child: ListView.builder(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    itemCount: _equipments.length,
-                    itemBuilder: (context, index) {
-                      final equipment = _equipments[index];
-                      return Padding(
-                        padding: const EdgeInsets.only(bottom: 20.0),
-                        child: EquipmentCard(
-                          type: equipment.model,
-                          brand: equipment.brand,
-                          model: equipment.model,
-                          color: equipment.color,
-                          serialNumber: equipment.serial,
-                          onEdit: () {
-                            // Lógica para editar equipo
-                          },
-                          onDeactivate: () {
-                            // Lógica para desactivar equipo
-                          },
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const AddEquipmentPage(),
                         ),
                       );
                     },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF39A900),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: const Text(
+                      "Agregar equipo",
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 35),
-              ],
+                ],
+              ),
             ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: _equipments.isEmpty
+                  ? _buildEmptyEquipmentView() // Muestra vista alternativa si no hay equipos
+                  : _buildEquipmentList(), // Muestra la lista si hay equipos
+            ),
+            const SizedBox(height: 35),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEmptyEquipmentView() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(
+            Icons.devices_other,
+            size: 80,
+            color: Colors.grey,
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            "No tienes equipos registrados.",
+            style: TextStyle(fontSize: 18, color: Colors.grey),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const AddEquipmentPage(),
+                ),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color(0xFF39A900),
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            child: const Text(
+              "Agregar equipo",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildEquipmentList() {
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      itemCount: _equipments.length,
+      itemBuilder: (context, index) {
+        final equipment = _equipments[index];
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 20.0),
+          child: EquipmentCard(
+            type: equipment.model,
+            brand: equipment.brand,
+            model: equipment.model,
+            color: equipment.color,
+            serialNumber: equipment.serial,
+            onEdit: () {
+              // Lógica para editar equipo
+            },
+            onDeactivate: () {
+              // Lógica para desactivar equipo
+            },
+          ),
+        );
+      },
     );
   }
 }
